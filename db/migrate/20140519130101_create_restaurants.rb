47ff708509908526be084c332c1fad82910bbfe4
Sroop <sroopsunar@hotmail.com>
2014-05-19 14:06:00 +0100

    restaurant model created with table columns defined in one command. next error, need to add a button to the form

---
+++ db/migrate/20140519130101_create_restaurants.rb
@@ -0,0 +1,10 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :cuisine
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140519130101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: true do |t|
+    t.string   "name"
+    t.string   "cuisine"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
