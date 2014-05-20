b2afc7ce5c3e4048f4fdf6857de79db22275ff81
Sroop <sroopsunar@hotmail.com>
2014-05-20 11:41:59 +0100

    unit testing Restaurant model now for validating data input before saving to the database. first test to make sure Restaurant name is not left blank

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Restaurant do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	context 'It shows an error' do
+
+			it 'when the name is blank' do
+				restaurant = Restaurant.new(name: nil)
+				expect(restaurant).to have(1).error_on(:name)
+			end
+
+	end
 end
