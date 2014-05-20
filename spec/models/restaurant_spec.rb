ccf1f842e7bbc28ad2ac2099efd2aaf891614acd
Sroop <sroopsunar@hotmail.com>
2014-05-20 11:46:57 +0100

    test for blank cuisine

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -4,9 +4,14 @@ describe Restaurant do
 
 	context 'It shows an error' do
 
+			let(:restaurant) { Restaurant.new(name: nil) }
+
 			it 'when the name is blank' do
-				restaurant = Restaurant.new(name: nil)
 				expect(restaurant).to have(1).error_on(:name)
+			end
+
+			it 'when the cuisine is blank' do
+				expect(restaurant).to have(1).error_on(:cuisine)
 			end
 
 	end
