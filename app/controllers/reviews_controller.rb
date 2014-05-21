4b3d89ee614c418bf2892ddf8ca79411e25f93f7
Sroop <sroopsunar@hotmail.com>
2014-05-21 12:23:26 +0100

    tests green, css okay for now

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,7 @@ class ReviewsController < ApplicationController
 
   def create
   	  @restaurant = Restaurant.find( params[:restaurant_id] )
-  	  @restaurant.reviews.create(params[:review].permit(:thoughts, :rating))
+  	  @restaurant.reviews.create(params[:review].permit(:title, :thoughts, :rating))
   	  redirect_to('/restaurants')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140521103242) do
+ActiveRecord::Schema.define(version: 20140521110944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140521103242) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "restaurant_id"
+    t.string   "title"
   end
 
   add_index "reviews", ["restaurant_id"], name: "index_reviews_on_restaurant_id", using: :btree
