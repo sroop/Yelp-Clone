5772651ce4fb390ef8ea11fb38cc5c89fb0ce63d
Sroop <sroopsunar@hotmail.com>
2014-05-21 20:00:57 +0100

    made the ajax work. had to reshuffle some html around. title is currently being rendered via mustache. need to sort rest of the content

--- app/assets/javascripts/restaurants.js
+++ app/assets/javascripts/restaurants.js
@@ -2,8 +2,13 @@ $(document).ready(function() {
 	$('.new_review').on('submit', function(event) {
 		event.preventDefault();
 
+			var siblingList = $(this).siblings('ul');
+
 			$.post($(this).attr('action'), $(this).serialize(), function(response) {
-				console.log(response);
+				
+				var template = $('#ajax-reviews').html();
+					var rendered = Mustache.render(template, response);
+						siblingList.append(rendered);
 
 			}, 'json');
 	
