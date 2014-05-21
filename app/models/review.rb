3ca3602a1641e7da1b658b5811d2d350bf83575e
Sroop <sroopsunar@hotmail.com>
2014-05-21 12:35:54 +0100

    validation added to verify the correct rating is selected between 1 and 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ActiveRecord::Base
 
 	belongs_to :restaurant
-	
+
+	validates :rating, inclusion: { in: [*1..5] }
+
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Review do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	context 'validating the rating' do
+
+		it 'can rate a restaurant no higher than 5' do
+			review = Review.new(title: "old street", thoughts: "average", rating: 10)
+			expect(review).to have(1).error_on(:rating)
+		end
+
+		it 'can rate a restaurant no lower than 1' do
+			review = Review.new(title: "old street", thoughts: "average", rating: 0)
+			expect(review).to have(1).error_on(:rating)
+		end
+
+	end
+
 end
